3c0eea98f5d5a8201d372ac8d3515ce5e4434d73
Alex Malykh <alex@fym.team>
2020-08-26 11:52:20 +0700

    Almost finished: 📖React 🔥Real-time database - firebase 🌍Hosted online - https://todo-app-745fd.web.app ✅Full CRUD functionality (Create, Read, Update & Delete) ✅Material UI for the design

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDnywzDoaL_2OJdttBdAmCZaoILmt4DY-I",
+    authDomain: "todo-app-745fd.firebaseapp.com",
+    databaseURL: "https://todo-app-745fd.firebaseio.com",
+    projectId: "todo-app-745fd",
+    storageBucket: "todo-app-745fd.appspot.com",
+    messagingSenderId: "328905272207",
+    appId: "1:328905272207:web:feace4e072d635560e39fa",
+    measurementId: "G-609QK062L4"
+});
+
+const db = firebaseApp.firestore();
+
+export default db;
